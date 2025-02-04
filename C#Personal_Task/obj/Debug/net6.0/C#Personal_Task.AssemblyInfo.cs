1337373f48f552ba49bd87746a834cce10a4ab98
Jujeongmin <anjshdkdl99@gmail.com>
2025-02-04 09:14:27 +0900

    gitignore

--- C#Personal_Task/obj/Debug/net6.0/C#Personal_Task.AssemblyInfo.cs
+++ C#Personal_Task/obj/Debug/net6.0/C#Personal_Task.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Personal_Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfebcc777bda542c7639fc14a5642c82967a561")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Personal_Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Personal_Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
